a840e1d02d525842c56777843419437d74dc2d1c
Sayed <sayedshehata33@gmail.com>
2024-10-15 10:43:01 +0300

    feat: :card_file_box: Make Task interface and Create Tasks Service

--- src/app/Services/task.service.ts
+++ src/app/Services/task.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../Models/Task';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private API_URL = 'http://localhost:3000/tasks';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // Get all tasks
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.API_URL);
+  }
+
+  // Create new task
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.API_URL, task);
+  }
+
+  // Toggle Task Completion
+  toggleCompletion(task: Task): Observable<Task> {
+    console.log('Task completed:', task);
+    return this.http.put<Task>(`${this.API_URL}/${task.id}`, {
+      ...task,
+      completed: !task.completed,
+    });
+  }
+
+  // Delete a task
+  deleteTask(id: string): Observable<void> {
+    console.log('Task deleted:', id);
+    return this.http.delete<void>(`${this.API_URL}/${id}`);
+  }
 }
