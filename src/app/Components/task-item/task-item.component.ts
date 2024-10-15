6d3d9703e6af1d11f85726964c81c0a989d6a3ab
Sayed <sayedshehata33@gmail.com>
2024-10-15 11:58:55 +0300

    feat: :sparkles: Create TaskItem Component

--- src/app/Components/task-item/task-item.component.ts
+++ src/app/Components/task-item/task-item.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../../Models/Task';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.css'
+  styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
+  @Input() task!: Task;
+  @Output() toggle = new EventEmitter<Task>();
+  @Output() delete = new EventEmitter<string>();
 
+  toggleCompletion(): void {
+    this.toggle.emit(this.task);
+  }
+  deleteTask(): void {
+    this.delete.emit(this.task.id);
+  }
 }
